0a2bb9b47b49e8134a5d55ac861a9b0cf6c1fd41
Przemyslaw <misiek440@gmail.com>
2022-10-31 08:55:08 +0100

    add input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,22 @@ import { Quotation } from './models/quote';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  title: string = '';
   quotes: Quotation[] = QUOTES;
   addButton: boolean = false;
 
+  quotation: Quotation = {
+    author: '',
+    quotation: '',
+    votes: 0,
+  };
+
   countVote(addOdd: number, index: number) {
     this.quotes[index].votes += addOdd;
+  }
+
+  addQuotation() {
+    this.quotes.push(this.quotation);
+    this.quotation = { author: '', quotation: '', votes: 0 };
   }
 }
